b4aca547c50d3d70df2cc35e9d1e722e37ac08e1
Gaurav Prajapati <gauravprajapati9370@gmail.com>
2025-08-20 12:03:33 +0530

    user auth completed

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+dotenv.config();
+
+
+connectDB();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+
+export default app;
